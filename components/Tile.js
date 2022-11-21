1a3e2867851b443faf1da40053ea63970130e512
StijnVP <stijnvanpeborgh@gmail.com>
2022-11-21 10:43:43 +0100

    added pressCounter state to Tile component

--- components/Tile.js
+++ components/Tile.js
@@ -1,15 +1,18 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 const Tile = props => {
+  const [pressCounter, setPressCounter] = useState(0);
   function pressHandler() {
     console.log("pressed " + props.title);
+    setPressCounter((currentPressCounter) => currentPressCounter + 1);
   }
   return (
-    <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={() => pressHandler()}>
+    <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={pressHandler}>
       {console.log("render " + props.title)}
       <View>
         <Text style={styles.tileHeader}>{props.title}</Text>
-        <Text style={styles.tileText}>count goes here</Text>
+        <Text style={styles.tileText}>{pressCounter}</Text>
       </View>
     </TouchableOpacity>
   );
